38e2864e88bb147e473d287d7ffb04e9d534794c
Nekuxd <71673861+Nekuxd@users.noreply.github.com>
2021-06-06 03:31:50 -0400

    Comentario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-
+//._.
 class UserController extends Controller
 {
     /**
