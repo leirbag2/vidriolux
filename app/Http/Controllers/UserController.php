24ed79fa7673e4d4ac7b38ccba9d79b967caabfb
Gabriel Maturana <gabrielmaturana1999@hotmail.com>
2021-06-11 02:30:51 -0400

    No se puede crear un usuario que tenga permisos sin tener un tipo de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+
 class UserController extends Controller
 {
     //Define los permisos para acceder a cada ruta
@@ -72,7 +73,9 @@ class UserController extends Controller
         $usuario->tipo_estado_id = $tipo_estado;
         $usuario->save();
         $usuario->roles()->sync($request->roles);
-        $usuario->syncPermissions($request->permissions);
+        if ($request->roles != null) {
+            $usuario->syncPermissions($request->permissions);
+        }
         return redirect("/usuarios")->with('info', 'Se creó el usuario correctamente');
     }
 
