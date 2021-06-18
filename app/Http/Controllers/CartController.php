158dd7e11f0d17907b49a4168d1147c4e02a4b98
Nekuxd <71673861+Nekuxd@users.noreply.github.com>
2021-06-18 02:53:58 -0400

    ._.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -27,7 +27,7 @@ class CartController extends Controller
 
         $id = $request->get('id');
         $producto = Productos::find($id);
-        $cantidad = $request->input('cantidad');
+        $cantidad = $request->input('cantidad') != null ? ($request->input('cantidad') > 0 ? $request->input('cantidad') : 1): 1;
         $precioVenta = $request->input('venta');
         $producto->precioVenta = $precioVenta;
         $oldCart =  $request->session()->has('cart') ? $request->session()->get('cart') : null;
