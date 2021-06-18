99b7b8babee555dc1a63950da5f27f8f79768a42
Gabriel Maturana <gabrielmaturana1999@hotmail.com>
2021-06-18 03:10:22 -0400

    Mensaje bienvenido

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -8,10 +8,9 @@
     </div>
 
     <div class="mt-6 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ratione autem possimus illo omnis nisi veritatis
-        ex sequi? Quae dignissimos harum atque voluptatum unde magnam omnis aliquid natus blanditiis velit.m
+        Bienvenido al sistema de bodega de VidrioLux.
     </div>
-    <img src="{{asset('images/home.jpg')}}" alt="" class="w-full shadow-lg rounded-2xl">
+    <img src="{{asset('images/home.jpg')}}" alt="" class="mt-4 w-full shadow-lg rounded-2xl">
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
